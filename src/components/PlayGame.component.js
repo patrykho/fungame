84f8295812b67ad8acf4c8cb88326f472ee93c8a
Patrick Holody <patryk.holody@gmail.com>
2019-09-03 16:58:20 +0200

    update

--- src/components/PlayGame.component.js
+++ src/components/PlayGame.component.js
@@ -1,14 +1,71 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Redirect } from 'react-router-dom'
 
-const PlayGame = (props) => {
+import randomNumber from '../method/randomNumber.method'
 
-    const {logged} = props
+const PlayGame = (props) => {
+const {logged} = props
+const [status, setStatus] = useState(false)
+const [userNumber, setUserNumber] = useState()
+const [guessNumber, setGuessNumber] = useState()
+const [lable, setLabel] = useState("Clik button to Start New Game")
+
+const handleInputChange = (event)=> {
+event.preventDefault()
+setUserNumber(parseInt(event.target.value,10))
+
+} 
+
+const handleStart = (event) => {
+    event.preventDefault()
+    setGuessNumber(randomNumber(100))
+    setLabel("")
+    setStatus(true)
+}
+
+const handleShot = (event) =>{ 
+    event.preventDefault()
+    if(userNumber){
+        if(userNumber > guessNumber) {
+            setLabel(`Target between 0 - ${userNumber-1}`)
+        }
+
+       else  if(userNumber < guessNumber) {
+            setLabel(`CTarget between ${userNumber+1} - 100`)
+        }
+        else if(userNumber == guessNumber ) {
+            setLabel("Congratulations Hit. End of the game!")
+            setStatus(false)
+            setUserNumber()
+        }
+    }
+
+}
+
+const renderUserInterface = () => {
+    return (<>
+
+          {/* <h1>Guess Number: {guessNumber}</h1> */}
+         <h1>Your Number: {userNumber}</h1>
+       <form onSubmit={handleShot}>
+        <label htmlFor="userNumber">Guess number:</label>
+    <input onChange={handleInputChange} type="number" id="userNumber" name="userNumber" min="0" max="100"/>
+    <button type="submit" >Shot</button>
+    </form>
+    </>)
+}
+
+
+  
 
     return (<>
-        {!logged ? (<Redirect exact from="/login" to="/" />) : (<p>Play</p>)}
-</>
-    )
+    {!logged &&<Redirect exact from="/login" to="/" />}
+   {lable&&<h1>Message: {lable}</h1>} 
+        {status ? (renderUserInterface()): (<><button onClick={handleStart}>Start New Game</button></>)}
+        </>)
+        
+        
+  
 }
 
 export default PlayGame
---
+++ src/method/randomNumber.method.js
@@ -0,0 +1,9 @@
+
+
+const randomNumber = (randomTo) => {
+
+    return Math.floor(Math.random() * randomTo)
+    
+}
+
+export default randomNumber
